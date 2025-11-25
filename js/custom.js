a22b700dd4235e4426c4fff5cd3b45b722a2f054
woel0721 <15609679967@163.com>
2025-11-25 14:51:50 +0800

    Site updated: 2025-11-25 14:51:50

--- js/custom.js
+++ js/custom.js
@@ -58,18 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
       selectors: ['title', '.content'],
       cacheBust: false
     });
-
-    document.addEventListener('pjax:send', function () {
-      document.body.classList.add('pjax-leave-active');
-    });
-
-    document.addEventListener('pjax:complete', function () {
-      document.body.classList.remove('pjax-leave-active');
-      document.body.classList.add('pjax-enter-active');
-      setTimeout(function () {
-        document.body.classList.remove('pjax-enter-active');
-      }, 500);
-    });
+    
   }
 
   // 切换主题的示例代码
